1736cd55dfe0164bac0e185db945ba20b78bdfa7
Sarah <ks.zerguerras@esi-sba.dz>
2026-06-30 16:50:26 +0100

    Complete project foundation

---
+++ proxy.ts
@@ -0,0 +1,21 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpg|jpeg|gif|png|svg|ico|woff2?|ttf)).*)",
+    "/(api|trpc)(.*)",
+    "/__clerk/:path*",
+  ],
+};
